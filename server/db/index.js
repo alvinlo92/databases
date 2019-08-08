f52a12b0c4f2ca37507c47f156ad748fefed5071
alvinlo92 <alvinlo92@yahoo.com>
2019-08-07 20:30:49 -0700

    Connect server to database

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,30 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  database : 'food',
+  user     : 'student',
+  password : 'student'
+})
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id' + connection.threadId);
+});
+
+connection.query('SELECT name FROM foods', function (error, results, fields) {
+  if (error)
+    throw error;
+
+  results.forEach(result => {
+    console.log(result);
+  });
+});
+
+connection.end();
 
 
--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,9 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id INT AUTO_INCREMENT PRIMARY KEY,
+  message VARCHAR(255),
+  user_id INT
 );
 
 /* Create other tables and define schemas for them here! */
